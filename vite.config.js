2a20b20d2b0705f6510c144e10f1ec53cb7471c5
cesarinsalad <cesgarcia1710@gmail.com>
2026-04-11 10:09:19 -0400

    feat: agregar capacidades de PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()]
-  ,
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false // La PWA real solo funciona en produccion (npm run build)
+      },
+      manifest: {
+        name: 'Mantenimiento App',
+        short_name: 'Mantenimiento',
+        description: 'Plataforma administrativa de equipos y mantenimientos',
+        theme_color: '#0B1121',
+        background_color: '#F8F9FA',
+        display: 'standalone', // Obliga al celular a esconder la barra superior de Chrome/Safari
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
-
-
